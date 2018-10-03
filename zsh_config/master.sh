affce2ba58dc0fc7782238ac9021a84659c5d7dc
Ian Eaves <ieaves@cibotechnologies.com>
2018-10-03 14:02:13 -0500

    post zsh setup script to handle activating conda

--- zsh_config/master.sh
+++ zsh_config/master.sh
@@ -6,3 +6,4 @@ source $CONFIG_DIR/aliases.sh
 source $CONFIG_DIR/tools.sh
 source $CONFIG_DIR/zplug.sh
 source $CONFIG_DIR/zsh_setup.sh
+source $CONFIG_DIR/post_zsh_setup.sh
--- zsh_config/tools.sh
+++ zsh_config/tools.sh
@@ -3,4 +3,3 @@ eval $(thefuck --alias)
 
 # Setting up conda
 . /Users/ieaves/miniconda3/etc/profile.d/conda.sh
-conda activate base
