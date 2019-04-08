74826420a5a719b25f7c71aec771f3289f318d45
Ian Eaves <ian.k.eaves@gmail.com>
2019-04-07 23:37:38 -0500

    comments

--- zsh_config/env_setup.sh
+++ zsh_config/env_setup.sh
@@ -1,3 +1,4 @@
+# These are required for pyenv
 export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
 export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
