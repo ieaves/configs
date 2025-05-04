7fa05ea23a141e98410b44a7aa4599c659ca992b
Ian Eaves <ian.k.eaves@gmail.com>
2025-05-04 11:24:29 -0500

    addin curosr alias

--- zsh_config/startup_scripts/aliases.sh
+++ zsh_config/startup_scripts/aliases.sh
@@ -8,7 +8,7 @@ alias ls=eza
 alias cat=bat
 alias pycharm='open -a /Applications/PyCharm.app'
 alias readlink='greadlink'
-
+alias cursor='open -a /Applications/Cursor.app'
 
 # as per https://github.com/pyenv/pyenv/issues/106
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
